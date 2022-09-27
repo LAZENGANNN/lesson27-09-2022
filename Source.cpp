780a26e8e234105762e19f4783966aa5a473dc7a
LAZENGANNN <113790875+LAZENGANNN@users.noreply.github.com>
2022-09-27 18:35:20 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,260 @@
+#include <iostream>
+using namespace std;
+int main() {
+	//if1.
+	/*int a;
+	cin >> a;
+	if (a > 0) {
+		a += 1; // a = a + 1
+	}
+	cout << a;*/
+
+	//if2.
+	/*int a;
+	cin >> a;
+	if (a > 0) {
+		a = a + 1;
+	}
+	else
+	{
+		a = a - 2;
+	}
+	cout << a;*/ 
+
+	//if3.
+	/*int a;
+	cin >> a;
+	if (a > 0) 
+	{
+		a++;
+	}
+	else if (a < 0)
+	{
+		a -= 2;
+	}
+	else if (a == 0)
+	{
+		a = 10;
+	}
+	cout << a;*/
+
+	//if4
+	/*int a;
+	int b;
+	int c;
+	int k = 0;
+	cin >> a >> b >> c;
+	if (a > 0)
+	{
+		k++;
+	}
+	if (b > 0)
+	{
+		k++;
+	}
+	if (c > 0)
+	{
+		k++;
+	}
+	
+	cout << k;*/
+
+	//if5.
+	/*int a;
+	int b;
+	int c;
+	int k = 0;
+	int k1 = 0;
+	cin >> a >> b >> c;
+	if (a > 0)
+	{
+		k++;
+	}
+	else
+	{
+		k1++;
+	}
+
+	if (b > 0)
+	{
+		k++;
+	}
+	else
+	{
+		k1++;
+	}
+
+	if (c > 0)
+	{
+		k++;
+	}
+	else
+	{
+		k1++;
+	}
+
+	cout << k << endl << k1;*/
+
+	//if6
+	/*int a;
+	int b;
+	cin >> a >> b;
+	if (a > b)
+	{
+		cout << a;
+	}
+	else if ( a < b)
+	{
+		cout << b;
+	}
+	else if (a == b)
+	{
+		cout << "=";
+	}*/
+
+    //if7.
+//b = 2
+//a = 1
+//int a;
+//int b;
+//cin >> a >> b;
+//if (a > b)
+//{
+//	cout << 2;
+//}
+//else if (a < b)
+//{
+//	cout << 1;
+//}
+//else if (a == b)
+//{
+//	cout << "=";
+//}
+
+    //if8.
+//int a;
+//int b;
+//cin >> a >> b;
+//if (a > b)
+//{
+//	cout << a << " " << b;
+//}
+//else if (a < b)
+//{
+//	cout << b << " " << a;
+//}
+//else if (a == b)
+//{
+//	cout << "=";
+//}
+
+//if 9
+//int a;
+//int b;
+//cin >> a >> b;
+//if (a > b)
+//{
+//	int m = a;
+//	int l = b;
+//	int a = l;
+//	int b = m;
+//	cout << a << " " << b;
+//}
+//else if (a < b)
+//{
+//	int m = a;
+//	int l = b;
+//	int a = m;
+//	int b = l;
+//	cout << a << " " << b;
+//
+//}
+//else if (a == b)
+//{
+//	cout << "=";
+//}
+
+
+//if 10
+//int a;
+//int b;
+//cin >> a >> b;
+//if (a != b)
+//{
+//	int s = a + b;
+//	int b = s;
+//	int a = s;
+//	cout << a << " " << b;
+//}
+//else if (a == b)
+//{
+//	int a = 0;
+//	int b = 0;
+//	cout << a << " " << b;
+//}
+
+//if11
+//int a;
+//int b;
+//cin >> a >> b;
+//if (a != b)
+//{
+//	if (a > b)
+//	{
+//		int s = a;
+//		int a = s;
+//		int b = s;
+//		cout << a << " " << b;
+//	}
+//	else if (a < b)
+//	{
+//		int s = b;
+//		int a = s;
+//		int b = s;
+//		cout << a << " " << b;
+//	}
+//}
+//else if (a == b)
+//{
+//	int a = 0;
+//	int b = 0;
+//	cout << a << " " << b;
+//}
+
+//if12
+//int a;
+//int b;
+//int c;
+//cin >> a >> b >> c;
+//if (a > b&& a > c)
+//{
+//	cout << a;
+//}
+//else if (b > a&& b > c)
+//{
+//	cout << b;
+//}
+//else if (c > a&& c > b)
+//{
+//	cout << c;
+//}
+
+//if13
+//int a;
+//int b;
+//int c;
+//cin >> a >> b >> c;
+//if (a > b&& a < c)
+//{
+//	cout << a;
+//}
+//else if (b > a&& b < c)
+//{
+//	cout << b;
+//}
+//else if (c > a&& c < b)
+//{
+//	cout << c;
+//}
+
+}
